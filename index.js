e6bbeccd0ddbba150e878fc244c2d4a0ced5859a
Flor Tello <flor.26071994@gmail.com>
2017-06-14 14:01:45 -0500

    Agregando recursos

---
+++ index.js
@@ -0,0 +1,42 @@
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const levelup     = require('levelup');
+
+const app = express();
+const db  = levelup('./data', {valueEncoding: 'json'});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({ message: 'Hola soy la POKEAPI'});
+});
+
+router.get('/pokes',(req,res) => {
+  let movies = [];
+  db.createValueStream().on('data', (data) => {
+      movies.push(data);
+  }).on('end', _ => {
+      res.json(movies);
+  });
+});
+
+router.get('/pokes/:id',(req,res) => {
+  if (req.params.id) {
+    db.get(req.params.id,(err,movie) => {
+      if (err) return res.json({message: "Hubo un error al obtener el registro"});
+      res.json(movie);
+    });
+  }
+});
+
+app.use('/api',router);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('El server esta corriendo en el '+port+'!');
+});
---
+++ seed.js
@@ -0,0 +1,36 @@
+const levelup   = require('levelup');
+const leveldown = require('leveldown');
+const data      = require('./http://pokeapi.co/api/v2/pokemon-species/1/');
+
+// leveldown.destroy('./data', function (err) { console.log('BD Destruida') });
+
+const db  = levelup('./http://pokeapi.co/api/v2/pokemon-species/1/', {valueEncoding: 'json'});
+
+// data.movies.forEach((movie) => {
+//   const id = movie.nombre.toLowerCase().split(" ").join("-");
+//   db.put(id,movie,(err) => {
+//     console.log("pelicula: "+movie.nombre + " importada");
+//   });
+// });
+
+
+'use strict';
+
+
+const getJSON = (url, cb) => {
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+
+    if (xhr.status !== 200) {
+      return cb(new Error('Error loading JSON from ' + url + '(' + xhr.status + ')'));
+    }
+    console.log(xhr);
+    cb(null, xhr.response);
+  });
+
+  xhr.open('GET', 'http://pokeapi.co/api/v2/pokemon-species');
+  xhr.responseType = 'json';
+  xhr.send();
+};
